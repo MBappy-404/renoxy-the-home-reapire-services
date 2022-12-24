474e032745ba8ed9dbeb545f9004eb62e6ea0f2d
MBappy-404 <muhammadbappy989@gmail.com>
2022-12-24 23:48:42 +0530

    update

--- src/components/AllServices/ShowAllService.js
+++ src/components/AllServices/ShowAllService.js
@@ -9,10 +9,10 @@ const ShowAllService = ({ service }) => {
 
   const { description, price, logo, name, _id } = service;
   return (
-    <div className='mb-8'>
-      <div class="bg-gray-900 group shadow-lg w-full h-full transform hover:scale-105 transition duration-500 rounded p-3">
+    <div className=''>
+      <div class="bg-gray-900 group shadow-lg w-full h-full  rounded p-3">
         <div class=" relative">
-          <img class="w-full block rounded h-full lg:h-56" src={logo} alt=" " />
+          <img class="w-full block rounded transform hover:scale-zoom-105 relative transition duration-500 h-full lg:h-56" src={logo} alt=" " />
           <div class="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
             <button class=" text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition px-8"> <button className="btn  mt-2 btn-primary no-animation "> <Link to={`/serviceDetails/${_id}`}>View Details</Link> </button></button>
           </div>
@@ -21,7 +21,7 @@ const ShowAllService = ({ service }) => {
           <h3 class="text-white text-lg mb-2">{name}</h3>
           <p className='text-gray-400 mb-3'>{description.slice(0, 200)}...</p>
           <div className=''>
-            <p class="text-gray-200 font-semibold "> Service Fee: {price}</p>
+            <p class="text-gray-200 "> Service Fee: {price}</p>
           </div>
         </div>
       </div>
